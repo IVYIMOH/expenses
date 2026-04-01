07cbbbcab81e34166cce3de1f553f1be8ca8d899
iimoh <ivyimoh@gmail.com>
2026-04-01 11:02:50 +0300

    refactor:main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// main.jsx - Update this
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { AppProvider } from './contexts/AppContext'; // Add this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppProvider> {/* Add this wrapper */}
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
